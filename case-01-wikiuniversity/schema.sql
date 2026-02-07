b4863aac9568bebe913afc797a3eb4561694b5e4
nisanurmillah <nisanurmillah24@gmail.com>
2026-02-07 22:26:08 +0700

    Update and rename schema.sqlREADME.md to schema.sql

--- case-01-wikiuniversity/schema.sql
+++ case-01-wikiuniversity/schema.sql
@@ -1,6 +1,6 @@
 # data-analysis-sql-practice
 Kumpulan SQL Query untuk latihan Analisis Data menggunakan skema database e-commerce standar (mirip Northwind/Supabase)
 ##Skema Database 
-![Skema Database Latihan Analisis] (database-examples-schema-northwind.png)
+ (database-examples-schema-northwind.png)
 <img width="1053" height="541" alt="database-examples-schema-northwind" src="https://github.com/user-attachments/assets/e0d21d4e-6a61-45b7-b1aa-76545ac18bb0" />
 
